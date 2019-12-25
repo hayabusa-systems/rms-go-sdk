835d5781c736ef53eb252ad9b054ebb7e4d8f668
Hayato Nakamura <hyt.rugby@gmail.com>
2019-12-25 13:30:45 +0900

    余計な出力を削除

--- main_test.go
+++ main_test.go
@@ -8,7 +8,6 @@ import (
 
 func TestSearchOrder_引数なし(t *testing.T) {
 	a := RMSApi{}
-	t.Logf("Service_Secret : %s, License_Key : %s", os.Getenv("SERVICE_SECRET"), os.Getenv("LICENSE_KEY"))
 	a.Initialize(os.Getenv("SERVICE_SECRET"), os.Getenv("LICENSE_KEY"))
 	r, err := a.SearchOrder(3, time.Now().AddDate(0, 0, -7), time.Now().AddDate(0, 0, 1))
 	if err != nil {
