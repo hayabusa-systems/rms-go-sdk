3567439077e2fda7476f37ae0da72c59994d9642
Hayato Nakamura <hyt.rugby@gmail.com>
2020-02-10 11:56:12 +0900

    接続先をテスト環境に変更したため、テストケースを修正

--- main_test.go
+++ main_test.go
@@ -65,7 +65,7 @@ func TestSearchOrder_データ数を指定して検索(t *testing.T) {
 	a := RMSApi{}
 	a.Initialize(os.Getenv("SERVICE_SECRET"), os.Getenv("LICENSE_KEY"))
 	cond := SearchOrderCondition{}
-	cond.RequestRecordsAmount = 10
+	cond.RequestRecordsAmount = 2
 	cond.RequestPage = 1
 
 	r, err := a.SearchOrder(3, time.Now().AddDate(0, 0, -30), time.Now().AddDate(0, 0, 1), &cond)
@@ -74,8 +74,8 @@ func TestSearchOrder_データ数を指定して検索(t *testing.T) {
 		t.FailNow()
 	}
 
-	if len(r.OrderNumberList) != 10 {
-		t.Errorf("Error. epected 10, actual %d", len(r.OrderNumberList))
+	if len(r.OrderNumberList) != 2 {
+		t.Errorf("Error. epected 2, actual %d", len(r.OrderNumberList))
 	}
 }
 
@@ -98,8 +98,8 @@ func TestSearchOrder_containsArrayの含まれるテスト(t *testing.T) {
 	}
 
 	for _, o := range rd.OrderModelList {
-		if o.GetOrderSettlementModel.SettlementMethod != "クレジットカード" {
-			t.Errorf("Error. epected クレジットカード, actual %s", o.GetOrderSettlementModel.SettlementMethod)
+		if o.GetOrderSettlementModel.SettlementMethod != "代金引換" {
+			t.Errorf("Error. epected 代金引換, actual %s", o.GetOrderSettlementModel.SettlementMethod)
 		}
 	}
 }
@@ -175,16 +175,112 @@ func TestGetOrder_注文番号指定(t *testing.T) {
 	}
 }
 
-func TestGetOrder_クレジット分割払い(t *testing.T) {
+func TestUpdateOrderMemo_データ更新1(t *testing.T) {
+	dc := 1
+	dd := JsonDate{time.Now()}
+	st := 1
+	m := "hogefuga"
+	o := "hoge"
+	mps := "hoge"
+
 	a := RMSApi{}
 	a.Initialize(os.Getenv("SERVICE_SECRET"), os.Getenv("LICENSE_KEY"))
-	r, err := a.GetOrder([]string{os.Getenv("CARD_MULTI_PAY_ORDER_NUMBER")}, 3)
+
+	c := UpdateOrderMemoCondition{}
+	c.OrderNumber = os.Getenv("ORDER_NUMBER")
+	c.DeliveryClass = &dc
+	c.DeliveryDate = &dd
+	c.ShippingTerm = &st
+	c.Memo = &m
+	c.Operator = &o
+	c.MailPlugSentence = &mps
+	err := a.UpdateOrderMemo(&c)
 	if err != nil {
 		t.Errorf("Happend undefined error: %v", err)
 		t.FailNow()
 	}
-	t.Logf("%v\n", r)
+
+	r, err := a.GetOrder([]string{os.Getenv("ORDER_NUMBER")}, 3)
+	if err != nil {
+		t.Errorf("Happend undefined error: %v", err)
+		t.FailNow()
+	}
 	if r.GetOrderMessageModelList[0].MessageType != "INFO" {
 		t.Errorf("Happend error expected: INFO, acctual: %s", r.GetOrderMessageModelList[0].MessageType)
+	}
+	if dc != *r.OrderModelList[0].GetOrderDeliveryModel.DeliveryClass {
+		t.Errorf("Happend error expected: %d, acctual: %d", dc, *r.OrderModelList[0].GetOrderDeliveryModel.DeliveryClass)
+	}
+	ddStr1, _ := dd.MarshalJSON()
+	ddStr2, _ := r.OrderModelList[0].DeliveryDate.MarshalJSON()
+	if string(ddStr1) != string(ddStr2) {
+		t.Errorf("Happend error expected: %s, acctual: %s", ddStr1, ddStr2)
+	}
+	if st != *r.OrderModelList[0].ShippingTerm {
+		t.Errorf("Happend error expected: %d, acctual: %d", st, *r.OrderModelList[0].ShippingTerm)
+	}
+	if m != *r.OrderModelList[0].Memo {
+		t.Errorf("Happend error expected: %s, acctual: %s", m, *r.OrderModelList[0].Memo)
+	}
+	if o != *r.OrderModelList[0].Operator {
+		t.Errorf("Happend error expected: %s, acctual: %s", o, *r.OrderModelList[0].Operator)
+	}
+	if mps != *r.OrderModelList[0].MailPlugSentence {
+		t.Errorf("Happend error expected: %s, acctual: %s", mps, *r.OrderModelList[0].MailPlugSentence)
+	}
+}
+
+func TestUpdateOrderMemo_データ更新2(t *testing.T) {
+	dc := 0
+	dd := JsonDate{time.Now()}
+	st := 0
+	m := ""
+	o := ""
+	mps := ""
+
+	a := RMSApi{}
+	a.Initialize(os.Getenv("SERVICE_SECRET"), os.Getenv("LICENSE_KEY"))
+
+	c := UpdateOrderMemoCondition{}
+	c.OrderNumber = os.Getenv("ORDER_NUMBER")
+	c.DeliveryClass = &dc
+	c.DeliveryDate = &dd
+	c.ShippingTerm = &st
+	c.Memo = &m
+	c.Operator = &o
+	c.MailPlugSentence = &mps
+	err := a.UpdateOrderMemo(&c)
+	if err != nil {
+		t.Errorf("Happend undefined error: %v", err)
+		t.FailNow()
+	}
+
+	r, err := a.GetOrder([]string{os.Getenv("ORDER_NUMBER")}, 3)
+	if err != nil {
+		t.Errorf("Happend undefined error: %v", err)
+		t.FailNow()
+	}
+	if r.GetOrderMessageModelList[0].MessageType != "INFO" {
+		t.Errorf("Happend error expected: INFO, acctual: %s", r.GetOrderMessageModelList[0].MessageType)
+	}
+	if dc != *r.OrderModelList[0].GetOrderDeliveryModel.DeliveryClass {
+		t.Errorf("Happend error expected: %d, acctual: %d", dc, *r.OrderModelList[0].GetOrderDeliveryModel.DeliveryClass)
+	}
+	ddStr1, _ := dd.MarshalJSON()
+	ddStr2, _ := r.OrderModelList[0].DeliveryDate.MarshalJSON()
+	if string(ddStr1) != string(ddStr2) {
+		t.Errorf("Happend error expected: %s, acctual: %s", ddStr1, ddStr2)
+	}
+	if st != *r.OrderModelList[0].ShippingTerm {
+		t.Errorf("Happend error expected: %d, acctual: %d", st, *r.OrderModelList[0].ShippingTerm)
+	}
+	if r.OrderModelList[0].Memo != nil {
+		t.Errorf("Happend error expected: %s, acctual: %s", m, *r.OrderModelList[0].Memo)
+	}
+	if r.OrderModelList[0].Operator != nil {
+		t.Errorf("Happend error expected: %s, acctual: %s", o, *r.OrderModelList[0].Operator)
+	}
+	if r.OrderModelList[0].MailPlugSentence != nil {
+		t.Errorf("Happend error expected: %s, acctual: %s", mps, *r.OrderModelList[0].MailPlugSentence)
 	}
 }
